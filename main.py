8d887be12d1cc30c2743c978911893372c1299d9
Shailesh Aanand <anaandshailu@gmail.com>
2023-02-27 15:01:38 +0530

    use object instead of str

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ import faust
 from time import sleep
 
 print("sleeping...", flush=True)
-sleep(20)
+sleep(10)
 print("woke up.")
 
 app = faust.App("views_counter", broker="kafka://kafka:9092")
 
-topic = app.topic("views", value_type=str, value_serializer="raw")
+
+class PostView(faust.Record):
+    post_id: str
+    user_id: str
+
+
+topic = app.topic("views", value_type=PostView)
+post_views = app.Table("post_views", default=int)
 
 
 @app.agent(topic)
 async def processor(stream):
-    async for message in stream:
+    async for message in stream.group_by(PostView.post_id):
         print(f"Received : {message}", flush=True)
+        post_views[message.post_id] += 1
